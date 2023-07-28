b192014c6d9e2dee77dd652686fb8f027d97bd2d
ricomalangi <rikomalangi@gmail.com>
2023-07-28 21:13:07 +0800

    add database sql

--- pelanggaran.sql
+++ pelanggaran.sql
@@ -7,6 +7,50 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+DROP TABLE IF EXISTS `tb_bk`;
+CREATE TABLE `tb_bk` (
+  `id_bk` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) DEFAULT NULL,
+  `nama_bk` varchar(255) NOT NULL,
+  `nip` varchar(255) NOT NULL,
+  `alamat` text,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `agama` varchar(255) NOT NULL,
+  `tempat_lahir` varchar(255) DEFAULT NULL,
+  `tanggal_lahir` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_bk`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `tb_bk_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `tb_osis`;
+CREATE TABLE `tb_osis` (
+  `id_osis` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) DEFAULT NULL,
+  `nama_osis` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_osis`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `tb_osis_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `tb_prestasi`;
+CREATE TABLE `tb_prestasi` (
+  `id_prestasi` int(11) NOT NULL AUTO_INCREMENT,
+  `id_siswa` int(11) DEFAULT NULL,
+  `jenis_prestasi` varchar(20) NOT NULL,
+  `keterangan_prestasi` text,
+  `sertifikat` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_prestasi`),
+  KEY `id_siswa` (`id_siswa`),
+  CONSTRAINT `tb_prestasi_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `tb_siswa` (`id_siswa`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `tb_prestasi` (`id_prestasi`, `id_siswa`, `jenis_prestasi`, `keterangan_prestasi`, `sertifikat`) VALUES
+(1,	3,	'internasional',	NULL,	NULL),
+(2,	3,	'nasional',	NULL,	NULL);
+
 DROP TABLE IF EXISTS `tb_siswa`;
 CREATE TABLE `tb_siswa` (
   `id_siswa` int(11) NOT NULL AUTO_INCREMENT,
@@ -21,7 +65,7 @@ CREATE TABLE `tb_siswa` (
   `status` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id_siswa`),
   KEY `id_user` (`id_user`),
-  CONSTRAINT `tb_siswa_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`) ON DELETE SET NULL
+  CONSTRAINT `tb_siswa_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `tb_siswa` (`id_siswa`, `id_user`, `nama_siswa`, `nisn`, `tempat_lahir`, `tanggal_lahir`, `agama`, `alamat`, `jenis_kelamin`, `status`) VALUES
@@ -46,4 +90,4 @@ INSERT INTO `tb_user` (`id_user`, `username`, `password`, `role`) VALUES
 (5,	'sutrisman',	'$2y$10$cRgiM/99e0IMJPLGqWccB.IMJeloJpKy4iIz4FSoCpdJ3i.HYKQQ6',	'siswa'),
 (8,	'nurhadi123',	'$2y$10$/uMvyBG9UtRIHbBpZ938Fuh5dI0kG2krIDpXElN5YfIC92ZJgKTMS',	'siswa');
 
--- 2023-07-16 02:08:18
+-- 2023-07-28 13:12:27
